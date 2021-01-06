01708cdc2ef7db35a93bb65a065ea744c2671fd0
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-06 15:34:01 +0530

    Initial Commit

---
+++ Desserts App/Desserts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desserts_AppApp.swift
+//  Desserts App
+//
+//  Created by Mahesh Prasad on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Desserts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
